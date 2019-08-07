4a5d6646e8c7a97c7e52a7c1c7f4e707a52a7f1f
Suhas <suhaskn09@gmail.com>
2019-08-07 14:52:33 +0200

    fixing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
 
-port = process.env.DATABASE_URL || 3000
+port = process.env.PORT || 3000
 
 const app = express()
 
