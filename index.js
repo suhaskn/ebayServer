9403cea426c03b0a0caf6eec7f2141b6d7125e94
Suhas <suhaskn09@gmail.com>
2019-08-07 14:45:16 +0200

    Database connected

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
+const db = new Sequelize(databaseUrl)
+
+port = process.env.DATABASE_URL || 3000
+
+const app = express()
+
+db
+  .sync()
+  .then(() => console.log('Database schema updated'))
+  .catch(console.error)
+
+app.listen(port, ()=>console.log(`App listning on port:${port}`))
