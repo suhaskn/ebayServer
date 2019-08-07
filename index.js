9266ccaed45ab85a634547504d7cc66b383a1aba
Suhas <suhaskn09@gmail.com>
2019-08-07 16:24:11 +0200

    df

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 //const db = require('./db')
 //const Buyer = require('./buyer/index')
 const buyerRouter = require('./buyer/router')
 
 const app = express()
-const cors = require('cors')
+
 const middleware = cors()
 app.use(middleware)
 
